afd82c3af3ea89c1c8e3fdd3d4b838d2939f4226
njewell <njewell@xpanxion.com>
2018-08-07 09:54:19 -0500

    First commit for android project

---
+++ app/src/main/java/com/example/njewe/scythescorecalculator/DisplayActivityMessage.java
@@ -0,0 +1,14 @@
+package com.example.njewe.scythescorecalculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DisplayActivityMessage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+
+}
